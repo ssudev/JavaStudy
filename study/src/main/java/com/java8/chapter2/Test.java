a05f678c7a48df28b38129ce8822da37475ec856
ssudev <goodys1011@naver.com>
2021-04-14 21:28:17 +0900

    패키지 변경

---
+++ study/src/main/java/com/java8/chapter2/Test.java
@@ -0,0 +1,7 @@
+package com.java8.chapter2;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
--- study/src/test/java/com/java8/StudyApplicationTests.java
+++ study/src/test/java/com/java8/StudyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.java8.study;
+package com.java8;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
